b05ef061fcab6b40c21231b88ad92e58635101d8
見沢　健一 <kenichi.misawa@gmo.media>
2017-07-17 17:55:39 +0900

    プルダウンメニューの追加

--- script/script.js
+++ script/script.js
@@ -4,19 +4,21 @@ $(function() {
     function edit() {
         $('.inp').keyup(function() {
             var target_eq = $('#inps .inp').index(this);
-            var text = $('.inp').eq(target_eq).val()
+            var text = $('.inp').eq(target_eq).val();
             $('.prev').eq(target_eq).html(text);
         });
     }
 
     // inputタグ生成
-    var inp_area = '<span class="appended">お題：<input class="inp" type="text" value=""><button class="delete">×</button><br></span>';
-    var prev_area = '<span class="appended">お題：<span class="prev"></span><br></span>';
+    var extends_pulldown = '<select name="extends_menu" class="extends_menu"><option value="a">--- SNS ---</option><option value="a">twitter</option><option value="b">facebook</option><option value="c">google</option><option value="d">line</option></select>';
+    var inp_area = '<span class="appended">'+extends_pulldown+'：<input class="inp" type="text" value=""><button class="delete">×</button><br></span>';
+    var prev_area = '<span class="appended"><span class="menu">SNS</span>：<span class="prev"></span><br></span>';
     function add() {
         $('#add').click(function() {
             $('#inps').append(inp_area);
             $('#prevs').append(prev_area);
             edit();
+            extends_menu();
             death();
         });
     }
@@ -27,6 +29,15 @@ $(function() {
             var target_eq = $('#inps span .delete').index(this);
             $('#inps .appended').eq(target_eq).hide();
             $('#prevs .appended').eq(target_eq).hide();
+        });
+    }
+
+    // プルダウンメニューの反映
+    function extends_menu() {
+        $('.extends_menu').change(function() {
+            var target_eq = $('.extends_menu').index(this);
+            var extends_menu = $('[name=extends_menu] option:selected').eq(target_eq).text();
+            $('.menu').eq(target_eq).html(extends_menu);
         });
     }
 
