e76f418bff16307cb956d2c9b8a9f626316f36d6
見沢　健一 <kenichi.misawa@gmo.media>
2017-07-23 17:42:59 +0900

    画像描画

--- script/script.js
+++ script/script.js
@@ -5,12 +5,32 @@ $(function() {
     $(canvas).attr('width', '345px');
     $(canvas).attr('height', '209px');
 
+    //画像ロード
+    var image_array = new Array();
+    var image_name = ['../img/kty1.jpg', '../img/kty2.jpg', '../img/kty3.jpg'];
+    for (var d = 0; d < image_name.length; d++) {
+        image_array[d] = new Image();
+        image_array[d].src = image_name[d];
+    }
+
+    // テンプレート(画像)を描画
+    $('#background').change(function(){
+        var img = $('[name=background] option:selected').val();
+        $('canvas').drawImage({
+            draggable: true,
+            source: image_array[img],
+            x: 0, y: 0,
+            width: 345,
+            height: 209,
+            fromCenter: false
+        });
+    });
+
     // 文字を描画
     $('#add_text').click(function() {
         var text = $('#inp').val();
         var fontsize = $('[name=fontsize]:checked').val();
         var selectcolor = $('#color-list').val();
-
         $("canvas").drawText({
             draggable: true,
             fillStyle: selectcolor,
