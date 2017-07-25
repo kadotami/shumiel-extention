3c3eb8ae4516d0874033d6fecf378d2fddb4b74f
見沢　健一 <kenichi.misawa@gmo.media>
2017-07-25 23:43:08 +0900

    一旦

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,6 @@
 $(function() {
     // canvasを定義
+    num = 1;
     canvas_front = document.getElementById('canvas_front');
     canvas_back = document.getElementById('canvas_back');
     $('#canvas_front, #canvas_back').attr('width', '500px');
@@ -10,16 +11,76 @@ $(function() {
 
         // 画像ロード
         var image_array = new Array();
-        var image_name = ['../img/kty1.jpg', '../img/kty2.jpg', '../img/kty3.jpg'];
+        var image_name = ['../img/templete01.png', '../img/templete02.png', '../img/templete03.png', '../img/templete04.png', '../img/templete05.png'];
         for (var i = 0; i < image_name.length; i++) {
             image_array[i] = new Image();
             image_array[i].src = image_name[i];
         }
 
+        function card_back_create() {
+            // 名刺の裏をレイアウトする
+            var token = items.public_token;
+            $("#canvas_back").drawImage({
+                source: '../img/templete0'+num+'.png',
+                x: 0, y: 0,
+                width: 500,
+                height: 300,
+                fromCenter: false,
+            });
+
+            // AR項目に必要なもの
+            $("#canvas_back").drawImage({
+                draggable: true,
+                crossOrigin: 'anonymous',
+                source: 'http://chart.apis.google.com/chart?chs=400x400&cht=qr&chl=https://shumiel.modern-min.net/?token='+token,
+                x: 30, y: 30,
+                width: 200,
+                height: 200,
+                fromCenter: false,
+            });
+
+            $("#canvas_back").drawImage({
+                draggable: true,
+                source: '../img/ar_model.png',
+                x: 230, y: 30,
+                width: 200,
+                height: 200,
+                fromCenter: false,
+            });
+
+            if (num == 1 || num == null || num == 0) {
+                 $("#canvas_front").drawImage({
+                    source: '../img/templete01.png',
+                    x: 0, y: 0,
+                    width: 500,
+                    height: 300,
+                    fromCenter: false,
+                });
+            }
+
+        }
         // テンプレート(画像)を描画
         $('#background').change(function() {
-            var img = $('[name=background] option:selected').val();
-            console.log(image_array[img]);
+            num = $('[name=background] option:selected').val();
+            $('#canvas_front').css('background-image', 'url(../img/templete0'+num+'.png)');
+            $("#canvas_front").drawImage({
+                source: '../img/templete0'+num+'.png',
+                x: 0, y: 0,
+                width: 500,
+                height: 300,
+                fromCenter: false,
+            });
+
+/*            $("#canvas_back").drawImage({
+                draggable: true,
+                source: image_name[num],
+                x: 30, y: 30,
+                width: 200,
+                height: 200,
+                fromCenter: false,
+            });*/
+
+            /*console.log(image_array[img]);
             $('#canvas_front').drawImage({
                 draggable: true,
                 source: image_array[img],
@@ -27,10 +88,7 @@ $(function() {
                 width: 500,
                 height: 300,
                 fromCenter: false,
-                /*dblclick: function(layer) {
-                    $(this).removeLayer(layer);
-                }*/
-            });
+            });*/
         });
 
         // 文字を描画
@@ -38,15 +96,12 @@ $(function() {
             var text = $('#inp').val();
             var fontsize = $('#fontsize').val();
             var selectcolor = $('#color-list').val();
-            console.log(text);
-            console.log(fontsize);
-            console.log(selectcolor);
             $("#canvas_front").drawText({
                 draggable: true,
                 fillStyle: selectcolor,
                 strokeWidth: "0",
-                x: 100,
-                y: 100,
+                x: 120,
+                y: 120,
                 fontSize: fontsize,
                 fontFamily: "sans-serif",
                 text: text,
@@ -59,7 +114,6 @@ $(function() {
         // 画像を描画する
         $('#add_image').click(function() {
             var name = $('#upload_image')[0].files[0].name;
-            alert(name);
             $('#canvas_front').drawImage({
                 draggable: true,
                 source: name,
@@ -73,20 +127,12 @@ $(function() {
             });
         });
 
-        // 名刺の裏をレイアウトする
-        var token = items.public_token;
-        $("#canvas_back").drawImage({
-            draggable: true,
-            crossOrigin: 'anonymous',
-            source: 'http://chart.apis.google.com/chart?chs=400x400&cht=qr&chl=https://shumiel.modern-min.net/?token='+token,
-            x: 30, y: 30,
-            width: 200,
-            height: 200,
-            fromCenter: false,
-        });
-
         // canvas画像化
         $('#prev').click(function() {
+            // カードの裏をcanvasに描画する
+            card_back_create();
+
+            // カードをダウンロードするまでを行う
             var image_src_front = canvas_front.toDataURL("image/png");
             var image_src_back = canvas_back.toDataURL("image/png");
             console.log(image_src_front);
@@ -97,22 +143,23 @@ $(function() {
             $('#image_download_front').attr('href', image_src_front);
             $('#image_download_back').attr('href', image_src_back);
             $('#modal-download').fadeIn(600);
-            $('.cancel').click(function() {
-                $('#modal-download').fadeOut(600);
-            });
 
-            // ダウンロードイベント
-            $('.download').click(function() {
-                $('#image_download_front')[0].click();
-                $('#image_download_back')[0].click();
-            });
         });
     });
+
+    // クリックイベント系
     $('.help-contents').click(function() {
         $('#modal-tutorial').fadeIn(600);
     });
 
+    $('.cancel').click(function() {
+        $('#modal-download').fadeOut(600);
+    });
     $('.next').click(function() {
         $('#modal-tutorial').fadeOut(600);
+    });
+    $('.download').click(function() {
+        $('#image_download_front')[0].click();
+        $('#image_download_back')[0].click();
     });
 }); // $(function()
